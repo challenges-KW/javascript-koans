27055faac2fc2650b9fec24a26045d5d3727dcc5
katarzyna_w <katarzyna.koch@gmail.com>
2021-01-29 21:08:46 -0600

    Added solutions for AboutFunctions.js

--- koans/AboutFunctions.js
+++ koans/AboutFunctions.js
@@ -96,7 +96,6 @@ describe("About Functions", function() {
     praiseSinger.givePraise = appendDoubleRules;
     expect(praiseSinger.givePraise("Mary")).toBe("Mary totally rules!");
 
-console.log("yay");
 
   });
 });
