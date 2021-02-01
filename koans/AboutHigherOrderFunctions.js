8a60c975308ee9be38ff43ddd9625e961c34ebfd
katarzyna_w <katarzyna.koch@gmail.com>
2021-02-01 15:26:23 -0600

    Added solutions for first part of AboutHigherOrderFunctions.js

--- koans/AboutHigherOrderFunctions.js
+++ koans/AboutHigherOrderFunctions.js
@@ -9,28 +9,28 @@ describe("About Higher Order Functions", function () {
 
   it("should use filter to return array items that meet a criteria", function () {
     var numbers = [1,2,3];
-    var odd = _(numbers).filter(function (x) { return x % 2 !== 0 });
+    var odd = (numbers).filter(function (x) { return x % 2 !== 0 });
 
-    expect(odd).toEqual(FILL_ME_IN);
-    expect(odd.length).toBe(FILL_ME_IN);
-    expect(numbers.length).toBe(FILL_ME_IN);
+    expect(odd).toEqual([1, 3]);
+    expect(odd.length).toBe(2);
+    expect(numbers.length).toBe(3);
   });
 
   it("should use 'map' to transform each element", function () {
     var numbers = [1, 2, 3];
-    var numbersPlus1 = _(numbers).map(function(x) { return x + 1 });
+    var numbersPlus1 = (numbers).map(function(x) { return x + 1 });
 
-    expect(numbersPlus1).toEqual(FILL_ME_IN);
-    expect(numbers).toEqual(FILL_ME_IN);
+    expect(numbersPlus1).toEqual([2, 3, 4]);
+    expect(numbers).toEqual([1, 2, 3]);
   });
 
   it("should use 'reduce' to update the same result on each iteration", function () {
     var numbers = [1, 2, 3];
-    var reduction = _(numbers).reduce(
+    var reduction = (numbers).reduce(
             function(/* result from last call */ memo, /* current */ x) { return memo + x }, /* initial */ 0);
 
-    expect(reduction).toBe(FILL_ME_IN);
-    expect(numbers).toEqual(FILL_ME_IN);
+    expect(reduction).toBe(6);
+    expect(numbers).toEqual([1, 2, 3]);
   });
 
   it("should use 'forEach' for simple iteration", function () {
--- koans/practice.js
+++ koans/practice.js
@@ -32,25 +32,64 @@
 // console.log(aPerson.firstname);
 
 
-function Person(firstname, lastname)
-{
-  var fullName = firstname + " " + lastname;
+// function Person(firstname, lastname)
+// {
+//   var fullName = firstname + " " + lastname;
 
-  this.getFirstName = function () { return firstname; };
-  this.getLastName = function () { return lastname; };
-  this.getFullName = function () { return fullName; };
-}
+//   this.getFirstName = function () { return firstname; };
+//   this.getLastName = function () { return lastname; };
+//   this.getFullName = function () { return fullName; };
+// }
 
-var aPerson = new Person ("John", "Smith");
+// var aPerson = new Person ("John", "Smith");
 
-aPerson.firstname = "Penny";
-aPerson.lastname = "Andrews";
-aPerson.fullName = "Penny Andrews";
+// aPerson.firstname = "Penny";
+// aPerson.lastname = "Andrews";
+// aPerson.fullName = "Penny Andrews";
 
-aPerson.getFullName = function () {
-  return aPerson.lastname + ", " + aPerson.firstname;
+// aPerson.getFullName = function () {
+//   return aPerson.lastname + ", " + aPerson.firstname;
+// };
+
+// console.log(aPerson.firstname);
+// console.log(aPerson.getFirstName());
+// console.log((aPerson.getFullName()));
+
+
+function nFun () {
+  var numbers = [1, 2, 3];
+  var odd = (numbers).filter(function (x) { return x % 2 !== 0 });
 };
 
-console.log(aPerson.firstname);
-console.log(aPerson.getFirstName());
-console.log((aPerson.getFullName()));
+var numbers = [1, 2, 3];
+var odd = (numbers).filter(function (x) { return x % 2 !== 0 });
+
+console.log(odd.length);
+console.log(numbers.length);
+console.log(odd);
+
+
+
+function mappingFun () {
+  var numbers = [1, 2, 3];
+  var numbersPlus1 = (numbers).map(function(x) { return x + 1 });
+};
+
+
+var numbers = [1, 2, 3];
+var numbersPlus1 = (numbers).map(function(x) { return x + 1 });
+
+console.log(numbersPlus1);
+
+
+
+function redMemo () {
+  var numbers = [1, 2, 3];
+  var reduction = (numbers).reduce(
+          function(memo, x) { return memo + x }, 0);
+        };
+
+var reduction = (numbers).reduce(function(memo, x) { return memo + x }, 0);
+
+
+  console.log(reduction);
