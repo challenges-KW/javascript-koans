2206d0ce0a21f5972eb82a9fd6631272a51aa3d1
katarzyna_w <katarzyna.koch@gmail.com>
2021-02-01 09:20:50 -0600

    Added solutions for AboutMutability.js

--- koans/practice.js
+++ koans/practice.js
@@ -1,20 +1,56 @@
+// // var megalomaniac = {
+// //   mastermind : "Brain",
+// //   henchman: "Pinky",
+// //   battleCry: function (noOfBrains) {
+// //     return "They are " + this.henchman + " and the" +
+// //       Array(noOfBrains + 1).join(" " + this.mastermind);
+// //   }
+// // };
+// // console.log(megalomaniac.battleCry(4))
+
+
+
 // var megalomaniac = {
-//   mastermind : "Brain",
-//   henchman: "Pinky",
-//   battleCry: function (noOfBrains) {
-//     return "They are " + this.henchman + " and the" +
-//       Array(noOfBrains + 1).join(" " + this.mastermind);
-//   }
-// };
-// console.log(megalomaniac.battleCry(4))
+//     mastermind: "The Monarch",
+//     theBomb: true
+//   };
+
+// var hasBomb = "henchwoman" in megalomaniac;
+
+// console.log(hasBomb)
 
 
+// function Person(firstname, lastname)
+// {
+//   this.firstname = firstname;
+//   this.lastname = lastname;
+// }
 
-var megalomaniac = {
-    mastermind: "The Monarch",
-    theBomb: true
-  };
+// var aPerson = new Person ("John", "Smith");
+// aPerson.firstname = "Alan";
 
-var hasBomb = "henchwoman" in megalomaniac;
+// console.log(aPerson.firstname);
 
-console.log(hasBomb)
+
+function Person(firstname, lastname)
+{
+  var fullName = firstname + " " + lastname;
+
+  this.getFirstName = function () { return firstname; };
+  this.getLastName = function () { return lastname; };
+  this.getFullName = function () { return fullName; };
+}
+
+var aPerson = new Person ("John", "Smith");
+
+aPerson.firstname = "Penny";
+aPerson.lastname = "Andrews";
+aPerson.fullName = "Penny Andrews";
+
+aPerson.getFullName = function () {
+  return aPerson.lastname + ", " + aPerson.firstname;
+};
+
+console.log(aPerson.firstname);
+console.log(aPerson.getFirstName());
+console.log((aPerson.getFullName()));
